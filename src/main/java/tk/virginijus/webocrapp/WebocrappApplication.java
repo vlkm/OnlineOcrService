4dd1f3c9f524908ebee5dac7654bf8489a69c353
vlkm <46622384+vlkm@users.noreply.github.com>
2019-09-03 15:26:39 +0300

    success tesseract file v4 "eng" in directory "lib" pom tesseract v4.0.0 file from dir files/OA.png

--- src/main/java/tk/virginijus/webocrapp/WebocrappApplication.java
+++ src/main/java/tk/virginijus/webocrapp/WebocrappApplication.java
@@ -11,18 +11,18 @@ public class WebocrappApplication {
 
     public static void main(String[] args) {
         String inputPath  =  "target/classes/";
+        String inputFilePath = inputPath + "files/OA.png";
         Tesseract tesseract = new Tesseract();
         try {
             tesseract.setDatapath(inputPath + "lib");
-
-          //  tesseract.setLanguage("eng");
+         // tesseract.setLanguage("eng");
          // tesseract.setLanguage("lit");
-            String fullText  = tesseract.doOCR(new File(inputPath + "OA.png"));
+            String fullText  = tesseract.doOCR(new File(inputFilePath));
             System.out.println(fullText);
         } catch (TesseractException e) {
             e.printStackTrace();
         }
-        SpringApplication.run(WebocrappApplication.class, args);
+       // SpringApplication.run(WebocrappApplication.class, args);
     }
 
 }
