0f547ecf3d53ca82a2d40344923a8d880ce08e20
vlkm <46622384+vlkm@users.noreply.github.com>
2019-09-10 15:47:40 +0300

    mkDir added + "/" more print to screan

--- src/main/java/tk/virginijus/webocrapp/controller/UploadController.java
+++ src/main/java/tk/virginijus/webocrapp/controller/UploadController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import tk.virginijus.webocrapp.WebocrappApplication;
 import tk.virginijus.webocrapp.service.ImageReadService;
 
 import java.io.File;
@@ -18,8 +19,8 @@ import java.util.Date;
 @Controller
 public class UploadController {
 
-    //folder for the uploaded images
-    private static String UPLOADED_FOLDER = "/images/";
+    //Start folder for the uploaded images and libraries
+    private static String UPLOADED_FOLDER = "";
 
     @GetMapping("/")
     public String index() {
@@ -30,6 +31,38 @@ public class UploadController {
     public String singleFileUpload(@RequestParam("inputName") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
+        String inputPath  =  "target/classes/";
+//        file:/root/webocrapp-0.0.1.war!/WEB-INF/classes!/
+        String currentPath = WebocrappApplication.class
+                .getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .getPath()
+                ;
+        int i= 0;
+        //remove all till program folder webocrapp
+        System.out.println("Current Path get: " + currentPath);
+        i = currentPath.toLowerCase().lastIndexOf(":") + 1;
+        if (i > -1) { currentPath = currentPath.substring(i);
+        System.out.println("Current Path >: " + currentPath);}
+        i = currentPath.toLowerCase().lastIndexOf("webocrapp");
+        if (i >0 ) currentPath = currentPath.substring(0,i);
+        System.out.println("Current Path <: " + currentPath);
+        i = currentPath.toLowerCase().lastIndexOf("webocrapp");
+        if (i >0 ) { currentPath = currentPath.substring(0,i);
+        System.out.println("Current Path <: " + currentPath);}
+        //remove "/" at the end
+        String rentPath = currentPath;
+        if (rentPath.endsWith("/")) {
+            i = currentPath.length();
+            if (i>1) { i--;
+            currentPath = currentPath.substring(0,i);}
+        System.out.println("Current Path /: " + currentPath);}
+
+
+        UPLOADED_FOLDER = currentPath + "/images";
+        inputPath = UPLOADED_FOLDER + "/" + file.getOriginalFilename();
+
         if (file.isEmpty()) {
             redirectAttributes.addFlashAttribute("message", "Please select a image to upload");
             return "redirect:/";
@@ -37,22 +70,26 @@ public class UploadController {
 
         try {
             //Check the directory
-            File fileDir = new File(UPLOADED_FOLDER);
+            File fileDir = new File(UPLOADED_FOLDER + "/");
             if (!fileDir.exists()) {
                 if (fileDir.mkdir()) {
                     System.out.println("Directory is created! :" + UPLOADED_FOLDER);
                 } else {
                     System.out.println("Failed to create directory! :" + UPLOADED_FOLDER );
                 }
+            } else {
+                System.out.println("Used directory! :" + UPLOADED_FOLDER );
             }
-            // Get the file and save it somewhere
+            // Get the file and save it
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Path path = Paths.get(inputPath);
+            System.out.println("Image :" + path);
             Files.write(path, bytes);
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
             ImageReadService imageReadService = new ImageReadService();
-            String text = imageReadService.readImage(path);
+            String text = imageReadService.readImage(currentPath + "/lib" , path.toString());
             redirectAttributes.addFlashAttribute(  "text",  text);
             redirectAttributes.addFlashAttribute(  "time",  "Time : " + new Date());
 
--- src/main/java/tk/virginijus/webocrapp/service/ImageReadService.java
+++ src/main/java/tk/virginijus/webocrapp/service/ImageReadService.java
@@ -4,48 +4,27 @@ package tk.virginijus.webocrapp.service;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.springframework.stereotype.Component;
-import tk.virginijus.webocrapp.WebocrappApplication;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
 
 @Component
 public class ImageReadService{
 
-    public String readImage(Path inputFilePath) {
+    public String readImage(String libFolder, String inputFilePath) {
 
         String fullText = "";
-        String inputPath  =  "target/classes/";
-//         = WebocrappApplication.class.getResource("").getPath();
-        final String currentPath = WebocrappApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        System.out.println(currentPath);
-//        to get on server libraries
-//        if (currentPath == "/root/") {
-//
-//            inputPath = currentPath + "lib";
-//             if (!Files.exists(inputPath)) {
-//             try {
-//                    Files.createDirectories(inputPath);
-//                    System.out.println("Directory is creating! :" + inputPath);
-//                 } catch (IOException e) {
-//                 //fail to create directory
-//                    e.printStackTrace();
-//                    System.out.println("Failed to create directory! :" + inputPath );
-//                 }
-//             }
-//            inputPath = currentPath;
-//        }
-
-//      String inputFilePath = inputPath + "files/OA.png";
-            Tesseract tesseract = new Tesseract();
+        Tesseract tesseract = new Tesseract();
             try {
-                tesseract.setDatapath(inputPath + "lib");
-                //https://github.com/tesseract-ocr/tesseract/wiki/Data-Files
+                System.out.println("Used libFolder! :" + libFolder );
+                tesseract.setDatapath( libFolder );
+//more languages https://github.com/tesseract-ocr/tesseract/wiki/Data-Files
 //              tesseract.setLanguage("eng");
                 tesseract.setLanguage("lit");
-                fullText  = tesseract.doOCR(new File(inputFilePath.toString()));
+                File imageToRead = new File(inputFilePath);
+                if (imageToRead.exists()) {
+                    System.out.println("Read image! :" + imageToRead );
+                    fullText  = tesseract.doOCR(imageToRead);
+                }
                 System.out.println(fullText);
             } catch (TesseractException e) {
                 e.printStackTrace();
